96d2f605d74b86c92bb08a0701454cd21eb3ffff
Anna Perkowska <perkowska.a@gmail.com>
2018-01-26 11:17:39 +0100

    Change index.html, script.js, package.json

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var a = prompt('Podaj wartość a'),
+	b = prompt('Podaj wartość b'),
+	value;
+value = (a*a) - (2*a*b) + (b*b);
+if (value > 0) {
+	alert('wynik dodatni')
+	console.log('wynik dodatni');
+} else if (value < 0) {
+	alert('wynik ujemny')
+	console.log('wynik ujemny');
+} else {
+	alert('Wynik to 0')
+	console.log('Wynik to 0');
+}
